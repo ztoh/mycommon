26bc6a9714f7681601d64c8b5a84692e5adb0592
ztoh <bolonzhang@gmail.com>
2016-05-21 20:09:11 +0800

    2.0.4   测试EventManager中自定义调用扩展点

--- listenable/src/test/java/com/chnye/yese/common/able/test/TestEventLisenable.java
+++ listenable/src/test/java/com/chnye/yese/common/able/test/TestEventLisenable.java
@@ -2,18 +2,24 @@ package com.chnye.yese.common.able.test;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.chnye.yese.common.able.IListener;
 import com.chnye.yese.common.able.other.EventManager;
+import com.chnye.yese.common.able.other.EventManager.EventNotifyFunctor;
 import com.chnye.yese.common.able.other.IEvent;
 import com.chnye.yese.common.dto.PhyEqp;
 import com.chnye.yese.common.dto.PhyEqpEvent;
 
 public class TestEventLisenable {
 
+	private static Logger logger =  LoggerFactory.getLogger( TestEventLisenable.class );
+	
 	private MyLogger  myLogger = null;
 	private MyAsyncMessageQueue  myAsyncMessageQueue = null;
 	private EventManager<IListener<IEvent<?>>, IEvent<?>>  eventManager = null;
+	private EventNotifyFunctor<IListener<IEvent<?>>, IEvent<?>> functor = null;
 	
 	@Before
 	public void init(){
@@ -21,6 +27,15 @@ public class TestEventLisenable {
 		myAsyncMessageQueue = new MyAsyncMessageQueue();
 		eventManager = new EventManager<IListener<IEvent<?>>, IEvent<?>>();
 		
+		functor = new EventNotifyFunctor<IListener<IEvent<?>>, IEvent<?>>() {
+			@Override
+			public void notify(IListener<IEvent<?>> listener, IEvent<?> event) {
+				// TODO Auto-generated method stub
+				logger.debug("======================begin" );
+				listener.notify( event );
+				logger.debug("======================end" );
+			}
+		};
 	}
 	
 	@Test
@@ -32,7 +47,8 @@ public class TestEventLisenable {
 		for( int i = 0; i < 10; i++ ){
 			PhyEqp  phyEqp = new PhyEqp( i, "phy" + i, "eqp" + i );
 			PhyEqpEvent event = new PhyEqpEvent( phyEqp );
-			eventManager.fireEvent( event );
+//			eventManager.fireEvent( event );
+			eventManager.fireEvent(functor, event);
 		}
 	}
 }
--- listenable/src/test/java/com/chnye/yese/common/able/test/TestEventLisenable2.java
+++ listenable/src/test/java/com/chnye/yese/common/able/test/TestEventLisenable2.java
@@ -2,9 +2,12 @@ package com.chnye.yese.common.able.test;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.chnye.yese.common.able.IListener;
 import com.chnye.yese.common.able.other.EventManager2;
+import com.chnye.yese.common.able.other.EventManager2.EventNotifyFunctor2;
 import com.chnye.yese.common.able.other.IEvent2;
 import com.chnye.yese.common.dto.PhyEqp;
 import com.chnye.yese.common.dto.PhyEqpEvent;
@@ -13,9 +16,12 @@ import com.chnye.yese.common.dto.ResSpec;
 
 public class TestEventLisenable2 {
 
+	private static Logger logger =  LoggerFactory.getLogger( TestEventLisenable2.class );
+	
 	private MyLogger2  myLogger2 = null;
 	private MyAsyncMessageQueue2  myAsyncMessageQueue2 = null;
 	private EventManager2<IListener<IEvent2<ResSpec,?>>, IEvent2<ResSpec,?>, ResSpec>  eventManager2 = null;
+	private EventNotifyFunctor2<IListener<IEvent2<ResSpec,?>>, IEvent2<ResSpec,?>> functor = null;
 	
 	@Before
 	public void init(){
@@ -23,6 +29,18 @@ public class TestEventLisenable2 {
 		myAsyncMessageQueue2 = new MyAsyncMessageQueue2();
 		eventManager2 = new EventManager2<IListener<IEvent2<ResSpec,?>>, IEvent2<ResSpec,?>, ResSpec>();
 		
+		functor = new EventNotifyFunctor2<IListener<IEvent2<ResSpec,?>>, IEvent2<ResSpec,?>>() {
+
+			@Override
+			public void notify(IListener<IEvent2<ResSpec, ?>> listener,
+					IEvent2<ResSpec, ?> event) {
+				// TODO Auto-generated method stub
+				if( event.getEventType() == ResSpec.OBD ){
+					logger.debug( "is OBD" );
+					listener.notify( event );
+				}
+			}
+		};
 	}
 	
 	@Test
@@ -35,8 +53,8 @@ public class TestEventLisenable2 {
 		for( int i = 0; i < 10; i++ ){
 			PhyEqp  phyEqp = new PhyEqp( i, "phy" + i, "eqp" + i );
 			PhyEqpEvent2 event2 = new PhyEqpEvent2(ResSpec.OBD, phyEqp);
-
-			eventManager2.fireEvent( event2 );
+//			eventManager2.fireEvent( event2 );
+			eventManager2.fireEvent(functor, event2);
 		}
 	}
 }
