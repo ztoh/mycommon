e981aa0c1ad923a727d54e0364102f005961e3fe
ztoh <bolonzhang@gmail.com>
2016-05-24 10:11:26 +0800

    该接口泛型的另一种写法

--- listenable/src/main/java/com/chnye/yese/common/able/IListenable.java
+++ listenable/src/main/java/com/chnye/yese/common/able/IListenable.java
@@ -1,5 +1,10 @@
 package com.chnye.yese.common.able;
 
+/** 
+写的更紧密的版本
+interface IListenable<E extends IEvent, L extends Listener<E>>{}
+ */
+
 public interface IListenable<L> {
 	void addListener( L listener );
 	void removeListener( L listener );
